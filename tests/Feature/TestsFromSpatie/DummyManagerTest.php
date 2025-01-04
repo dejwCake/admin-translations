4424e4da1de36f110ab532733f9edad61b831dd3
David Běhal <david.behal@websupport.sk>
2025-01-04 16:53:16 +0100

    Fix test

--- tests/Feature/TestsFromSpatie/DummyManagerTest.php
+++ tests/Feature/TestsFromSpatie/DummyManagerTest.php
@@ -6,31 +6,35 @@ namespace Brackets\AdminTranslations\Tests\Feature\TestsFromSpatie;
 
 use Brackets\AdminTranslations\Tests\Feature\TestsFromSpatie\TranslationManagers\DummyManager;
 use Brackets\AdminTranslations\Tests\TestCase;
+use Illuminate\Foundation\Application;
 
 class DummyManagerTest extends TestCase
 {
+    /**
+     * @param Application $app
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+     */
+    protected function getEnvironmentSetUp($app): void
+    {
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('admin-translations.translation_manager', DummyManager::class);
+    }
+
     public function testItAllowToChangeTranslationManager(): void
     {
-        $this->forceDummy();
         self::assertInstanceOf(DummyManager::class, $this->app['translation.loader']);
     }
 
     public function testItCanTranslateUsingDummyManagerUsingFile(): void
     {
-        $this->forceDummy();
         self::assertEquals('en value', trans('file.key'));
     }
 
     public function testItCanTranslateUsingDummyManagerUsingDb(): void
     {
-        $this->forceDummy();
         $this->createTranslation('*', 'file', 'key', ['en' => 'en value from db']);
 
         self::assertEquals('en value from db', trans('file.key'));
-    }
-
-    private function forceDummy(): void
-    {
-        app()->singleton('translation.loader', static fn ($app) => new DummyManager($app['files'], $app['path.lang']));
     }
 }
