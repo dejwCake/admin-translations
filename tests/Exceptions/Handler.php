fdff1bddd50b2328e7d16e20099c1b77a9c984d2
David Běhal <david.behal@team.blue>
2026-02-26 22:07:42 +0100

    Fix response call

--- tests/Exceptions/Handler.php
+++ tests/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
@@ -72,7 +73,7 @@ class Handler extends ExceptionHandler
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
+            return new JsonResponse(['error' => 'Unauthenticated.'], 401);
         }
 
         return redirect()->guest(route('login'));
